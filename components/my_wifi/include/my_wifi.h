15599f507598bf465c5faa7e75d3f2c1b6b2f256
Dainius Vaiksnys <dainius.vaiksnys@staffmatch.com>
2023-08-20 16:46:31 +0300

    first version

---
+++ components/my_wifi/include/my_wifi.h
@@ -0,0 +1,12 @@
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+bool wifi_init(void);
